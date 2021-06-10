9ab61ffee8fd64d24d73178121cfd19bfa1728d3
IzzelAliz <csh2001331@126.com>
2021-06-10 14:57:11 +0800

    Make Unit as Product0

--- src/main/java/io/izzel/tools/Unit.java
+++ src/main/java/io/izzel/tools/Unit.java
@@ -1,10 +1,33 @@
 package io.izzel.tools;
 
-public enum Unit {
+import io.izzel.tools.func.Func;
+import io.izzel.tools.func.Func0;
+import io.izzel.tools.product.Product;
+
+public enum Unit implements Product {
     INSTANCE;
 
     @Override
     public String toString() {
         return "Unit";
+    }
+
+    @Override
+    public <R> R map(Func<R> func) {
+        if (func instanceof Func0) {
+            return ((Func0<R>) func).apply();
+        } else {
+            return func.applyArray();
+        }
+    }
+
+    @Override
+    public Object productElement(int i) throws IndexOutOfBoundsException {
+        throw new IndexOutOfBoundsException();
+    }
+
+    @Override
+    public int productArity() {
+        return 0;
     }
 }
